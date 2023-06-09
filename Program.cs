e7a9df3a192ac315a81ead450727abe8f1c96cdd
GeneralGilmar <gaspar.general1987@gmail.com>
2023-06-09 15:22:49 +0100

    Tratando erros com exeções personalizado

--- Program.cs
+++ Program.cs
@@ -3,12 +3,22 @@ using tabuleiro;
 using xadrez_console;
 using xadrez;
 
-Tabuleiro tab = new Tabuleiro(8, 8);
+
+
+try
+{
+    Tabuleiro tab = new Tabuleiro(8, 8);
 
 
 
-tab.ColocarPeca(new Torre(tab, Cor.preta),new Posicao(0,0));
-tab.ColocarPeca(new Torre(tab, Cor.preta), new Posicao(1, 3));
-tab.ColocarPeca(new Rei(tab, Cor.preta), new Posicao(2, 4));
+    tab.ColocarPeca(new Torre(tab, Cor.preta), new Posicao(0, 9));
+    tab.ColocarPeca(new Torre(tab, Cor.preta), new Posicao(1, 3));
+    tab.ColocarPeca(new Rei(tab, Cor.preta), new Posicao(1, 5));
 
-Tela.ImprimirTabuleiro(tab);
+    Tela.ImprimirTabuleiro(tab);
+
+}
+catch(TabuleiroException e)
+{
+    Console.WriteLine(e.Message);
+}
--- tabuleiro/Tabuleiro.cs
+++ tabuleiro/Tabuleiro.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tabuleiro;
 
 namespace tabuleiro
 {
@@ -27,12 +28,45 @@ namespace tabuleiro
             return Pecas[linha, coluna];
         }
 
+        public Peca Peca(Posicao pos)
+        {
+            return Pecas[pos.Linha,pos.Coluna];
+        }
+        public bool ExistePeca(Posicao pos)
+        {
+            ValidarPosicao(pos);
+        
+              return Peca(pos)!=null;
+        }
         public void ColocarPeca(Peca p,Posicao pos)
         {
-            Pecas[pos.Linha, pos.Coluna] = p;
-            p.Posicao = pos;
+            if (ExistePeca(pos))
+            {
+                throw new TabuleiroException("Já existe peça nesta Posição");
+            }
 
+            Pecas[pos.Linha, pos.Coluna] = p;
+           // p.Posicao = pos;
 
         }
+
+       public bool PosicaoValida(Posicao pos)
+        {
+            if(pos.Linha>=Linhas || pos.Linha<0 || pos.Coluna>Colunas || pos.Coluna<0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void ValidarPosicao(Posicao pos)
+        {
+            if (!PosicaoValida(pos))
+            {
+                throw new TabuleiroException("Posiçao Invalida");
+            }
+        }
+
+
     }
 }
