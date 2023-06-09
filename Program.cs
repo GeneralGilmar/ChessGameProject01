558351613c1b2a6db861689107a90511d899b0fc
GeneralGilmar <gaspar.general1987@gmail.com>
2023-06-09 14:26:59 +0100

    começando a colocar peças no tabuleiro

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using tabuleiro;
 using xadrez_console;
+using xadrez;
 
 Tabuleiro tab = new Tabuleiro(8, 8);
 
 
+
+tab.ColocarPeca(new Torre(tab, Cor.preta),new Posicao(0,0));
+tab.ColocarPeca(new Torre(tab, Cor.preta), new Posicao(1, 3));
+tab.ColocarPeca(new Rei(tab, Cor.preta), new Posicao(2, 4));
 
 Tela.ImprimirTabuleiro(tab);
